f337b4c6f452ea92e111cb00fd442fb89d29a26c
senatormarina <senatormarina@gmail.com>
2023-02-26 20:15:43 +0400

    fix brain-progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -6,7 +6,7 @@ def get_question_and_answer():
     start_range = random.randint(1, 10)
     step_range = random.randint(1, 10)
     progression = list(range(start_range, 100, step_range))[0:11]
-    empty_index = random.randint(1, 10)
+    empty_index = random.randint(1, len(progression) - 1)
     number = progression[empty_index]
     progression[empty_index] = '..'
     return [progression, number]
