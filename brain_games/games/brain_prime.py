cdb751e38e7068a01781646f78a9448fea146dd5
Marina Senatorova <senatormarina@gmail.com>
2023-03-14 21:57:16 +0400

    fix brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -3,7 +3,7 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
-    number = random.randint(2, 99)
+    number = random.randint(0, 99)
 
     if is_prime(number):
         answer = 'yes'
@@ -15,6 +15,9 @@ def get_question_and_answer():
 
 
 def is_prime(number):
+    if (number == 0) or (number == 1):
+        return False
+
     divider = 2
     while number % divider != 0:
         divider += 1
